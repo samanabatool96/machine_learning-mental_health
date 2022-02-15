d04b2c8909da0c388e1d4c44b29ffeb7b81e8910
Samana Batool <39328412+samanabatool96@users.noreply.github.com>
2022-02-15 17:27:48 +0500

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -7,9 +7,9 @@ app=MultiApp()
 #with open("styles/style.css") as f:
    #st.markdown(f'<style>{f.read()}</style>',unsafe_allow_html=True)
 side_bar=st.container()
-col2 = st.container()
-
-col2.image('images/0001.jpg')
+with main_page:
+   col2 = st.container()
+   col2.image('images/0001.jpg')
 
   
 with side_bar:
